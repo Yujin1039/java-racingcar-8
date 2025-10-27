1a582c0b34db79eef1a5ce3488cfe8fcd667a06d
Yujin1039 <shelly103989@gmail.com>
2025-10-27 21:34:39 +0900

    feat: 차수별 실행 결과 포맷 구현

--- src/main/java/racingcar/OutputFormatter.java
+++ src/main/java/racingcar/OutputFormatter.java
@@ -1,8 +1,24 @@
 package racingcar;
 
-public class OutputFormatter {
-    // 차수별 실행 결과
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
+public class OutputFormatter {
+    private final StringBuilder sb = new StringBuilder("실행결과\n");
+
+    // 차수별 실행 결과
+    public void getRoundRecord(Map<String, Integer> carRecord){
+        Set<String> carNames = carRecord.keySet();
+        Iterator<String> carIter = carNames.iterator();
+
+        if(carIter.hasNext()){
+            String car = carIter.next();
+            String format = "-";
+            sb.append(car+" : "+format.repeat(carRecord.get(car))+"\n");
+        }
+        sb.append("\n");
+    }
 
     // 우승자 안내 문구
 }
