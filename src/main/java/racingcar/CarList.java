5638fabe968d93feba4ace85f4726b28d8c1b6d0
Yujin1039 <shelly103989@gmail.com>
2025-10-27 21:11:00 +0900

    feat: 자동차 목록 클래스 기본 설정 작성

--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -5,11 +5,26 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class CarList {
-    private Map<String, Integer> carRecord;
-    private int round;
+    private final Map<String, Integer> carRecord;
+    private final int round;
 
     // 생성자(Key: 차 이름, value: 전진 횟수)
+    public CarList(String[] carNames, int round){
+        this.carRecord = new HashMap<>();
+        this.round = round;
 
+        for(String car:carNames){
+            carRecord.put(car.trim(),0);
+        }
+    }
+
+    public Map<String, Integer> getCarRecord(){
+        return carRecord;
+    }
+
+    public int getRound(){
+        return round;
+    }
 
     // 자동차 이동값 저장
 
